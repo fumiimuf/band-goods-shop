9cb2eaa47592547807b7740599094ca9aae0c814
takafumi iwasaki <moz8boy@gmail.com>
2026-04-23 11:04:52 +0900

    修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE `user` (
   `is_admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT '管理者フラグ(0:一般, 1:管理者)',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-)
+);
 
 ------------------------------------------- 
 -- goodsテーブル関連
@@ -33,7 +33,7 @@ CREATE TABLE `goods` (
   `delete_date_time` datetime DEFAULT NULL COMMENT '削除日時',
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '削除フラグ(0:販売中, 1:削除済み)',
   PRIMARY KEY (`id`)
-)
+);
 
 ------------------------------------------- 
 -- categoryテーブル関連
@@ -42,7 +42,7 @@ CREATE TABLE `category` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `name` varchar(50) NOT NULL COMMENT 'カテゴリ名',
   PRIMARY KEY (`id`)
-)
+);
 
 ------------------------------------------- 
 -- cartテーブル関連
@@ -52,7 +52,7 @@ CREATE TABLE `cart` (
   `goods_id` int NOT NULL COMMENT 'グッズID',
   `quantity` int NOT NULL COMMENT '個数',
   PRIMARY KEY (`user_id`,`goods_id`)
-)
+);
 
 ------------------------------------------- 
 -- orderテーブル商品関連
@@ -66,7 +66,7 @@ CREATE TABLE `order` (
   `total_price` int NOT NULL COMMENT '合計金額',
   `ordered_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '購入日時',
   PRIMARY KEY (`id`)
-)
+);
 
 ------------------------------------------- 
 -- order_detailテーブル関連
@@ -79,5 +79,5 @@ CREATE TABLE `order_detail` (
   `ordered_price` int NOT NULL COMMENT '購入時価格',
   `quantity` int NOT NULL COMMENT '個数',
   PRIMARY KEY (`id`)
-)
+);
 
