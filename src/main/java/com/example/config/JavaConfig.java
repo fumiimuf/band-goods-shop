d88c51516f06b29f156b696adb540058596ff082
takafumi iwasaki <moz8boy@gmail.com>
2026-05-13 14:03:09 +0900

    commit

--- src/main/java/com/example/config/JavaConfig.java
+++ src/main/java/com/example/config/JavaConfig.java
@@ -4,12 +4,22 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+
 @Configuration
 public class JavaConfig {
 
+	// マッピングの機能
 	@Bean
 	public ModelMapper modelMapper() {
 		
 		return new ModelMapper();
+	}
+	
+	// レイアウトの機能
+	@Bean
+	public LayoutDialect layoutDialect() {
+		
+		return new LayoutDialect();
 	}
 }
