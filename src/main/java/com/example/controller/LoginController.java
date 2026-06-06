862891da59c72674dc184b95b6b73bfda32e7325
takafumi iwasaki <moz8boy@gmail.com>
2026-06-06 14:22:58 +0900

    Javaドックでコメントを追加しましたが、削除しています。内容は変更前と変わりません。

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+	
+	/**
+	 * ログイン画面を表示
+	 * @return ログイン画面のHTMLファイル名("login")
+	 */
 	@GetMapping("/login")
 	public String login() {
 		return "login";
