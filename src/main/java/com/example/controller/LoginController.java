1cb181c13f970a7811f0a81a8a5deb60bfde1145
takafumi iwasaki <moz8boy@gmail.com>
2026-06-26 15:36:08 +0900

    コメントを削除しました。

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-	
-	/**
-	 * ログイン画面を表示
-	 * @return ログイン画面のHTMLファイル名("login")
-	 */
 	@GetMapping("/login")
 	public String login() {
 		return "login";
