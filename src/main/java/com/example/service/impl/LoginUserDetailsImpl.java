e3a9080de0975d64ed37c3f2c67486f02011636f
takafumi iwasaki <moz8boy@gmail.com>
2026-07-04 13:26:05 +0900

    ファイルを移動
    ・LoginUserDetailsImplをimplパッケージへ移動

--- src/main/java/com/example/service/impl/LoginUserDetailsImpl.java
+++ src/main/java/com/example/service/impl/LoginUserDetailsImpl.java
@@ -1,4 +1,4 @@
-package com.example.service;
+package com.example.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
