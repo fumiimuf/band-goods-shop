9e45d1677f937e5596b1988aa67c8056db1e56c7
takafumi iwasaki <moz8boy@gmail.com>
2026-07-09 14:43:18 +0900

    refactor: OrderDetailMapperから不要になったselectByOrderIdメソッドを削除
    OrderMapper側でのJOIN（1対多）による一括取得へ移行したため、
    使用されなくなった selectByOrderId メソッドを削除しました。

--- src/main/java/com/example/repository/OrderDetailMapper.java
+++ src/main/java/com/example/repository/OrderDetailMapper.java
@@ -1,7 +1,5 @@
 package com.example.repository;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.entity.OrderDetail;
@@ -11,8 +9,4 @@ public interface OrderDetailMapper {
 
 	// 注文詳細情報を登録
 	int insert(OrderDetail orderDetail);
-	
-	// 注文IDからその注文の明細(子)を「すべて」取得する
-	List<OrderDetail> selectByOrderId(Integer userId);
-	
 }
