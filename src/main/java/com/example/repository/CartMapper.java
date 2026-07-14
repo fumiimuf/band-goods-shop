9299bc1f8d2a54d8dae27f166081ed7e10391276
takafumi iwasaki <moz8boy@gmail.com>
2026-07-14 14:24:58 +0900

    refactor: コメントの削除

--- src/main/java/com/example/repository/CartMapper.java
+++ src/main/java/com/example/repository/CartMapper.java
@@ -11,25 +11,17 @@ import com.example.model.CartItem;
 @Mapper
 public interface CartMapper {
 
-	// 現在のカートに同じ商品があるか確認する
 	Cart selectByUserIdAndGoodsId(@Param("userId") Integer userId, @Param("goodsId") Integer goodsId);
 	
-	// 新しく商品を登録する(個数1)
 	void insert(Cart cart);
 	
-	// すでにある商品の個数を増やす
 	void updateQuantity(Cart cart);
 	
-	// 【新しく追加】ユーザーIDを指定して、その人のカート情報をすべて取得する
-    // 戻り値は「商品名や価格」も含んだ CartItem のリストにします
 	List<CartItem> findByUserId(Integer userId);
 	
-	//ログインしているユーザーの特定のグッズを削除する
 	void deleteByGoodsId(@Param("userId") Integer userId, @Param("goodsId") Integer goodsId);
 	
-	// ログインユーザーのカート情報をすべて削除する
 	void deleteAllByUserId(Integer userId);
 	
-	// ユーザーIDに紐づくカート内の合計金額を取得する
 	int selectTotalAmountByUserId(Integer userId);
 }
