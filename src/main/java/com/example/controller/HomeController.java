df546b218eff65e1375281a786739f31cc6a6d34
takafumi iwasaki <moz8boy@gmail.com>
2026-07-30 15:09:57 +0900

    efactor: エラーハンドリング全体の設定強化およびアクセス制限処理の最適化
    - HomeController の /access-denied で設定するトーストメッセージキーを toast.error.notFound
    に変更

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -34,7 +34,7 @@ public class HomeController {
 	public String accessDenied(@AuthenticationPrincipal LoginUser loginUser,
 					RedirectAttributes redirectAttributes) {
 		
-		redirectAttributes.addFlashAttribute("toastError", "messages.propertiesで後で定義します。");
+		redirectAttributes.addFlashAttribute("toastError", "toast.error.notFound");
 		
 		if (loginUser == null) {
             return "redirect:/login";
