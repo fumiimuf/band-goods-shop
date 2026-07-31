175daa91de119cef567c29c5e9d7196a708a2545
takafumi iwasaki <moz8boy@gmail.com>
2026-07-31 15:28:55 +0900

    refactor: HomeControllerのLoginUserのimportパスを修正

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.config.LoginUser;
+import com.example.model.LoginUser;
 
 
 
