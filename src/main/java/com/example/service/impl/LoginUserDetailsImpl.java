40e8b217971a503a90371cac2381537c0e0b095e
takafumi iwasaki <moz8boy@gmail.com>
2026-07-31 15:32:42 +0900

    refactor: LoginUserDetailsImplのLoginUserのimportパスを修正

--- src/main/java/com/example/service/impl/LoginUserDetailsImpl.java
+++ src/main/java/com/example/service/impl/LoginUserDetailsImpl.java
@@ -7,8 +7,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.example.config.LoginUser;
 import com.example.entity.User;
+import com.example.model.LoginUser;
 import com.example.repository.UserMapper;
 
 @Service
